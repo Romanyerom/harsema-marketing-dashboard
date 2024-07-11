f7e7900ff40251d35d993603bc37aac26438e1fb
Romanyerom <Romanyerom@github.com>
2024-07-11 12:59:53 +0000

    Finalize production build setup and serve configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {},
   },
@@ -8,5 +7,5 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
 
